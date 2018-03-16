362541bce6a49a586762e99076905b796077d8e9
Iacopo Spalletti <i.spalletti@nephila.it>
2018-03-16 22:55:12 +0100

    Add queryset

--- panino/prenotazioni/views.py
+++ panino/prenotazioni/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Panino
+
 
 def lista_panini(request):
-    return render(request, 'prenotazioni/lista_panini.html', {})
+    panini = Panino.objects.all()
+    return render(request, 'prenotazioni/lista_panini.html', {'panini': panini})
