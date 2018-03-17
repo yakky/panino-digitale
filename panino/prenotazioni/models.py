2f2b5a712b8f4ed24a840e0fdeac4ec26a3802c2
Iacopo Spalletti <i.spalletti@nephila.it>
2018-03-17 08:52:19 +0100

    Added date of insertion to Panino

--- panino/prenotazioni/models.py
+++ panino/prenotazioni/models.py
@@ -8,6 +8,8 @@ class Panino(models.Model):
     nome = models.CharField(verbose_name=_('nome'), max_length=200)
     descrizione = models.TextField(verbose_name=_('descrizione'))
     prezzo = models.DecimalField(verbose_name=_('prezzo'), max_digits=4, decimal_places=2)
+    inserimento = models.DateTimeField(verbose_name=_('giorno inserimento'), auto_now_add=True, null=True)
+    disponibilita = models.DateTimeField(verbose_name=_('giorno disponibilità'), null=True)
 
     class Meta:
         verbose_name = _('panino')
